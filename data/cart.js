692a89d4abe858dfe00232ee41923cef84f13597
duckdoe <fortunefoluso@gmail.com>
2025-09-28 16:27:05 +0100

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-const cart = [
+let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -32,4 +32,16 @@ function addToCart(productId) {
   }
 }
 
-export { cart, addToCart };
+function deleteFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+}
+
+export { cart, addToCart, deleteFromCart };
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, deleteFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -39,7 +39,9 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+              matchingProduct.id
+            }">
               Delete
             </span>
           </div>
@@ -54,7 +56,7 @@ cart.forEach((cartItem) => {
               type="radio"
               checked
               class="delivery-option-input"
-              name="delivery-option-${productId}"
+              name="delivery-option-${matchingProduct.id}"
             />
             <div>
               <div class="delivery-option-date">Tuesday, June 21</div>
@@ -90,3 +92,11 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHtml;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    deleteFromCart(productId);
+    console.log(cart);
+  });
+});
