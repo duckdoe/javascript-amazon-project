013b4a166c6525edbdd402f0cabac31ffaf7a09d
duckdoe <fortunefoluso@gmail.com>
2025-09-28 16:39:48 +0100

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,21 @@
-let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+
+function saveToLocalStorage(name, data) {
+  localStorage.setItem(name, JSON.stringify(data));
+}
 
 function addToCart(productId) {
   let matchingItem;
@@ -30,6 +38,8 @@ function addToCart(productId) {
       quantity,
     });
   }
+
+  saveToLocalStorage("cart", cart);
 }
 
 function deleteFromCart(productId) {
@@ -42,6 +52,8 @@ function deleteFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToLocalStorage("cart", cart);
 }
 
 export { cart, addToCart, deleteFromCart };
